9c637e7afc6fab8cdda1e7062bc217e89a86f9e9
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-14 21:05:54 +0100

    use and in if/else

--- start.py
+++ start.py
@@ -13,6 +13,23 @@ import math
 # else:
 #     print("slabo to wyglada" + imie2 )
 
+print("Moge wejsc do klubu?")
+print("----------------------")
+imie = input("Jak masz na imie? ")
+wiek = int(input("Ile masz lat? "))
+kasa = int(input("Ile masz kasy przy sobie? "))
+
+
+if wiek >= 18 and kasa >= 50:
+    print("Zapraszam do srodka",imie)
+else:
+    print("Sorry nie spelniasz wymagan")
+
+
+################################################################################################################
+
+################################################################################################################
+
 print("Program pierwszy dzienny Pythonowy")
 
 numbs = randint(1,100)
@@ -34,9 +51,6 @@ print("Wygrana liczba to: ", odp)
 print("----------------------")
 
 ###################################################################################################################
-
-
-
 
 ####################################################################################################################
 
